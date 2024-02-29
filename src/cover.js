0958d813d5cfe6c78880312556ce7a67f53c77fc
pauleena phan <pauleena2002@gmail.com>
2024-02-28 23:20:13 -0800

    added cover page

--- src/cover.js
+++ src/cover.js
@@ -4,9 +4,7 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export const CoverPage = ({navigation}) =>{
-    // useEffect(()=>{
-    //     navigation.navigate('Create Account Page');
-    // }, [])//empty arr means run this once instead of every render
+
     return(
         <View style = {styles.container}>
             <TouchableOpacity style = {{flex: 1, justifyContent: "center"}} onPress = {()=>{
