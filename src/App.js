d91d6bb383268c0d8dd23f0184831e9e146a0618
pauleena phan <pauleena2002@gmail.com>
2024-03-01 09:57:13 -0800

    passed down islogged function

--- src/App.js
+++ src/App.js
@@ -1,44 +1,80 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage'; //global storage in react, data will stay even when the app is closed
 // import { createDatabase } from './database.js';
 import { CreateAccPage, LoginPage } from './account.js';
 import { LoadingPage } from './loading.js';
 import { CoverPage } from './cover.js';
 import { AddJournalEntryPage, JournalHomePage } from './journal.js';
 import { HomePage } from './homepage.js';
+import { UserInfoProvider, getUserInfo } from './userInfo.js';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  //initial value is false
+  const [isLogged, setLogged] = useState(false);
+  // const {setUserEmail} = getUserInfo();
 
+  const getUserStatus = async () =>{
+    try{
+      const status = await AsyncStorage.getItem("UserIsLoggedIn");
+      console.log("status " + status);
+      setLogged(status);
+    }catch(error){
+      console.log("error " + error);
+    }
+  }
+
+  // const updateUserEmail = async () =>{,............................
+  //   try{
+  //     const recievedEmail = await AsyncStorage.getItem("UserEmail");
+  //     console.log(recievedEmail);
+  //     setUserEmail(recievedEmail);
+  //   }catch(error){
+  //     console.log("error" + error);
+  //   }
+  // }
+
+  //everytime the app opens and renders we check is the user is logged in
+  useEffect(()=>{
+    getUserStatus();
+    // updateUserEmail();
+  })
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = "Cover Page" component = {CoverPage}></Stack.Screen>
-        <Stack.Screen 
-          name = "Login Page" 
-          component = {LoginPage} 
-          //animation removes the screen to slide from left to right
-          //gesture disables the user to allow from moving to the previous screen on swipe
-          options = {{headerBackVisible: false, animation: 'none', gestureEnabled: false }}>
-        </Stack.Screen>
-        <Stack.Screen 
-          name = "Create Account Page" 
-          component = {CreateAccPage} 
-          options = {{headerBackVisible: false, animation: 'none', gestureEnabled: false}}>
-        </Stack.Screen>
-        <Stack.Screen 
-          name = "Loading Page" 
-          component = {LoadingPage}
-          options = {{headerBackVisible: true, animation: 'none', gestureEnabled: false}}>
-        </Stack.Screen>
-        <Stack.Screen name = "Journal Home Page" component = {JournalHomePage}></Stack.Screen>
-        <Stack.Screen name = "Journal New Entry Page" component = {AddJournalEntryPage}></Stack.Screen>
-        <Stack.Screen name = "Home Page" component = {HomePage}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserInfoProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name = "Cover Page">
+            {/* don't need component since we are passing down a child prop instead */}
+            {props => <CoverPage {...props} isLogged={isLogged} />}
+          </Stack.Screen>
+          <Stack.Screen 
+            name = "Login Page" 
+            component = {LoginPage} 
+            //animation removes the screen to slide from left to right
+            //gesture disables the user to allow from moving to the previous screen on swipe
+            options = {{headerBackVisible: false, animation: 'none', gestureEnabled: false }}>
+          </Stack.Screen>
+          <Stack.Screen 
+            name = "Create Account Page" 
+            component = {CreateAccPage} 
+            options = {{headerBackVisible: false, animation: 'none', gestureEnabled: false}}>
+          </Stack.Screen>
+          <Stack.Screen 
+            name = "Loading Page" 
+            component = {LoadingPage}
+            options = {{headerBackVisible: true, animation: 'none', gestureEnabled: false}}>
+          </Stack.Screen>
+          <Stack.Screen name = "Journal Home Page" component = {JournalHomePage}></Stack.Screen>
+          <Stack.Screen name = "Journal New Entry Page" component = {AddJournalEntryPage}></Stack.Screen>
+          <Stack.Screen name = "Home Page" component = {HomePage}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserInfoProvider>
   );
 }
 
