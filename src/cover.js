77ca9ec41b327fdc5e9c63c465fdf456b40dfaae
pauleena phan <pauleena2002@gmail.com>
2024-03-01 09:58:28 -0800

    added islogged to check userstatus

--- src/cover.js
+++ src/cover.js
@@ -3,16 +3,22 @@ import { View, Text, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
-export const CoverPage = ({navigation}) =>{
+export const CoverPage = ({navigation, isLogged}) =>{
 
     return(
         <View style = {styles.container}>
             <TouchableOpacity style = {{flex: 1, justifyContent: "center"}} onPress = {()=>{
-                navigation.navigate('Login Page');
+                if(isLogged){
+                    navigation.navigate('Home Page');
+                }else{
+                    console.log("not logged in yet")
+                    navigation.navigate('Login Page');
+                }
+                
             }}>
-                <Text>
-                    Welcome to Mental Me
-                </Text>
+            <Text>
+                Welcome to Mental Me
+            </Text>
             </TouchableOpacity>
         </View>
     )
