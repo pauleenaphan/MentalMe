6ec3a9aed733ebfa89d0613de488763dd048284e
pauleena phan <pauleena2002@gmail.com>
2024-03-01 09:57:57 -0800

    added async storage to user email

--- src/account.js
+++ src/account.js
@@ -5,28 +5,31 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@fir
 
 import { auth } from '../firebase/index.js';
 import { styles } from './styles.js'; 
+import { getUserInfo } from './userInfo.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const auth1 = auth;
 //Login and account creation uses firebase authentication
 export const CreateAccPage = ({navigation}) => {
-    const [personalInfo, setPersonalInfo] = useState({
-        email: ' ',
-        password: ' ',
-    });
+    // const [personalInfo, setPersonalInfo] = useState({
+    //     email: ' ',
+    //     password: ' ',
+    // });
 
-    //while the user is inputting text it will update the current value of email to the value in the inputbox
-    const handleInfo = (titleValue, value) =>{
-        setPersonalInfo({
-            ...personalInfo,
-            [titleValue]: value
-        })
-    }
+    // //while the user is inputting text it will update the current value of email to the value in the inputbox
+    // const handleInfo = (titleValue, value) =>{
+    //     setPersonalInfo({
+    //         ...personalInfo,
+    //         [titleValue]: value
+    //     })
+    // }
+    const {userEmail, setUserEmail, userPassword, setUserPassword} = getUserInfo();
 
     //password should be at least 6 characters
     //has the check already that email is in use already
     const createAcc = async () =>{
         try{
-            await createUserWithEmailAndPassword(auth1, personalInfo.email, personalInfo.password);
+            await createUserWithEmailAndPassword(auth1, userEmail, userPassword);
             console.log("success creating new account");
             return true;
         }catch(error){
@@ -43,19 +46,21 @@ export const CreateAccPage = ({navigation}) => {
         <TextInput 
             placeholder = 'email'
             //gets user email from textinput and set the value to email
-            onChangeText = {(text) => handleInfo('email', text)}
+            onChangeText = {(text) => setUserEmail(text)}
         />
 
         <TextInput
             placeholder = "password"
-            onChangeText = {(text) => handleInfo('password', text)}
+            onChangeText = {(text) => setUserPassword(text)}
         />
 
         <Button
             title = "Submit (to create account)"
             onPress={async()=>{
                 if(await createAcc()){
-                    navigation.navigate('Loading Page');
+                    AsyncStorage.setItem("UserIsLoggedIn", JSON.stringify(true));
+                    AsyncStorage.setItem("UserEmail", JSON.stringify(userEmail));
+                    navigation.navigate('Home Page');
                 }else{
                     console.log('account error');
                 }
@@ -67,31 +72,28 @@ export const CreateAccPage = ({navigation}) => {
             title = "Login (go to login page)"
             onPress = {() => navigation.navigate('Login Page')}
         />
-        <Button
-          title = "go to main journal page (temp)"
-          onPress = {() => navigation.navigate('Journal Home Page')}
-        />
     </View>
     );
 };
 
 
 export const LoginPage = ({navigation}) =>{
-    const [personalInfo, setPersonalInfo] = useState({
-        email: ' ',
-        password: ' ' 
-    });
+    // const [personalInfo, setPersonalInfo] = useState({
+    //     email: ' ',
+    //     password: ' ' 
+    // });
 
-    const handleInfo = (name, text) =>{
-        setPersonalInfo({
-            ...personalInfo,
-            [name]: text
-        })
-    }
+    // const handleInfo = (name, text) =>{
+    //     setPersonalInfo({
+    //         ...personalInfo,
+    //         [name]: text
+    //     })
+    // }
+    const {userEmail, setUserEmail, userPassword, setUserPassword} = getUserInfo();
 
     const loginAcc = async () =>{
         try{
-            await signInWithEmailAndPassword(auth, personalInfo.email, personalInfo.password);
+            await signInWithEmailAndPassword(auth, userEmail, userPassword);
             console.log("successfully logged in")
             return true;
         }catch(error){
@@ -107,17 +109,19 @@ export const LoginPage = ({navigation}) =>{
             </Text>
             <TextInput
                 placeholder = "email"
-                onChangeText = {(text) => handleInfo('email', text)}
+                onChangeText = {(text) => setUserEmail(text)}
             />
             <TextInput
                 placeholder = "password"
-                onChangeText = {(text) => handleInfo('password', text)}
+                onChangeText = {(text) => setUserPassword(text)}
             />
             <Button
                 title = "Login"
                 onPress={async () =>{
                     if(await loginAcc()){
-                        navigation.navigate('Loading Page');
+                        AsyncStorage.setItem("UserIsLoggedIn", JSON.stringify(true));
+                        AsyncStorage.setItem("UserEmail", JSON.stringify(userEmail));
+                        navigation.navigate('Home Page');
                     }else{
                         console.log('login info is not correct');
                     }
@@ -129,4 +133,15 @@ export const LoginPage = ({navigation}) =>{
             />
         </View>
     )
+}
+
+export const getCurrEmail = async () =>{
+    try{
+        const email = await AsyncStorage.getItem("UserEmail");
+        console.log("email " + email);
+        return email;
+    }catch(error){
+        console.log("error" + error);
+        return null;
+    }
 }
