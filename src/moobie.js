7c96e46fad8bd48161dc5e2abd616d485dfb7321
pauleena phan <pauleena2002@gmail.com>
2024-03-11 21:19:04 -0700

    created file for moobie's closet and design

---
+++ src/moobie.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState } from "react";
+
+const MoobieContext = createContext();
+
+export const MoobieProvider = ({children}) =>{
+    const[bodyPart, setBodyPart] = useState({
+        head: require('../imgs/moobie_head/head1.png'),
+        body: require('../imgs/moobie_body/body1.png'),
+        feet: require('../imgs/moobie_feet/feet1.png')
+    })
+
+    const handlePart = (part, imgFile) =>{
+        setBodyPart({
+            ...bodyPart,
+            [part]: imgFile
+        })
+    }
+
+    return(
+        <MoobieContext.Provider value = {{bodyPart, handlePart}}>
+            {children}
+        </MoobieContext.Provider>
+    )
+}
+
+export const getMoobie = () => useContext(MoobieContext);
