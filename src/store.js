e2993194df2c6fd3afa81245056015f3dc36928e
pauleena phan <pauleena2002@gmail.com>
2024-03-11 22:35:21 -0700

    added store homepage

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { View, Text, Button, Image } from "react-native"
+
+import { styles } from "./styles.js";
+
+//Main home page 
+export const StorePage = () =>{
+    return(
+        <View style = {styles.container}>
+            <Text>
+                store
+            </Text>
+        </View>
+    );
+};
