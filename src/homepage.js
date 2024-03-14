812809f8483c734665661631cb60bc2ac0cd119c
pauleena phan <pauleena2002@gmail.com>
2024-03-13 17:10:51 -0700

    added button to closet

--- src/homepage.js
+++ src/homepage.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from "react";
 import { View, Text, Button, Image } from "react-native"
 
-import { moobie, styles } from "./styles.js";
+import { homePageMoobie, styles } from "./styles.js";
 import { getMoobie } from "./moobie.js";
 import { useFocusEffect } from "@react-navigation/native";
 
@@ -20,9 +20,9 @@ export const HomePage = ({navigation}) =>{
             <Text>
                 homepage
             </Text>
-            <Image source = {bodyPart.head} style = {moobie.moobie_head}/>
-            <Image source = {bodyPart.body} style = {moobie.moobie_body}/>
-            <Image source = {bodyPart.feet} style = {moobie.moobie_feet}/>
+            <Image source = {bodyPart.head} style = {homePageMoobie.moobie_head}/>
+            <Image source = {bodyPart.body} style = {homePageMoobie.moobie_body}/>
+            <Image source = {bodyPart.feet} style = {homePageMoobie.moobie_feet}/>
             <Button
                 title = "go to main journal page (temp)"
                 onPress = {() => navigation.navigate('Journal Home Page')}
@@ -34,6 +34,10 @@ export const HomePage = ({navigation}) =>{
             <Button
                 title = "store (temp)"
                 onPress = {() => navigation.navigate("Store Page")}
+            />
+            <Button
+                title = "Closet (temp)"
+                onPress = {() => navigation.navigate("Closet Page")}
             />
             <Button
                 title = "go to settings page (temp)"
