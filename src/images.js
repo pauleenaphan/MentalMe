1292112393b33d1ac78a29656525f3eb013ef004
pauleena phan <pauleena2002@gmail.com>
2024-03-13 22:40:34 -0700

    combined all img arr into one arr

---
+++ src/images.js
@@ -0,0 +1,16 @@
+export const images = {
+    headImgs: [
+        {name: 'Default Head', image: require('../imgs/moobie_head/head1.png'), imageP: ('../imgs/moobie_head/head1.png')},
+        {name: 'Detective Hat', image: require('../imgs/moobie_head/head2.png'), imageP: ('../imgs/moobie_head/head2.png')},
+        {name: 'Wicked Glasses', image: require('../imgs/moobie_head/head3.png'), imageP: ('../imgs/moobie_head/head3.png')},
+        {name: 'Shades', image: require('../imgs/moobie_head/head4.png'), imageP: ('../imgs/moobie_head/head4.png')},
+        {name: 'HoneyComb Ears', image: require('../imgs/moobie_head/head5.png'), imageP: ('../imgs/moobie_head/head4.png')}
+    ],
+    bodyImgs: [
+        {name: 'Gold Chain', image: require('../imgs/moobie_body/body2.png')},
+    ],
+    lowerBodyImgs: [
+        {name: 'Pink Bunny Slippers', image: require('../imgs/moobie_feet/feet2.png')},
+    ]
+
+};
