25ce94c1f313bc18dbde556b453c305300ca3245
pauleena phan <pauleena2002@gmail.com>
2024-03-18 20:11:11 -0700

    added prices to some of the items

--- src/images.js
+++ src/images.js
@@ -5,9 +5,9 @@ export const images = {
         {name: 'Default Lower Body', image: require('../imgs/moobie_feet/feet1.png'), imageP:('../imgs/moobie_feet/feet1.png')},
     ],
     headImgs: [
-        {name: 'Detective Hat', image: require('../imgs/moobie_head/head2.png'), imageP: ('../imgs/moobie_head/head2.png')},
-        {name: 'Wicked Glasses', image: require('../imgs/moobie_head/head3.png'), imageP: ('../imgs/moobie_head/head3.png')},
-        {name: 'Shades', image: require('../imgs/moobie_head/head4.png'), imageP: ('../imgs/moobie_head/head4.png')},
+        {name: 'Detective Hat', image: require('../imgs/moobie_head/head2.png'), imageP: ('../imgs/moobie_head/head2.png'), price: '2'},
+        {name: 'Wicked Glasses', image: require('../imgs/moobie_head/head3.png'), imageP: ('../imgs/moobie_head/head3.png'), price: '10'},
+        {name: 'Shades', image: require('../imgs/moobie_head/head4.png'), imageP: ('../imgs/moobie_head/head4.png'), price: '5'},
         {name: 'HoneyComb Ears', image: require('../imgs/moobie_head/head5.png'), imageP: ('../imgs/moobie_head/head4.png')}
     ],
     bodyImgs: [
