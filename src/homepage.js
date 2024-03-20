34af9cec571e5c1b12d2b585ef0890039c60f3b5
pauleena phan <pauleena2002@gmail.com>
2024-03-19 22:45:39 -0700

    added background testing

--- src/homepage.js
+++ src/homepage.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text, Button, Image } from "react-native"
+import { View, Text, Button, Image, TouchableOpacity, ImageBackground } from "react-native"
 import { useFocusEffect } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FontAwesome5, Feather, Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
 
-import { homePageMoobie, styles } from "./styles.js";
+import { homePageMoobie, styles, homePage } from "./styles.js";
 import { getMoobie } from "./moobie.js";
 import { getCurrency } from "./currency.js";
 
@@ -37,34 +38,83 @@ export const HomePage = ({navigation}) =>{
     )
 
     return(
-        <View style = {styles.container}>
-            <Text>
-                homepage
-            </Text>
-            <Image source = {bodyPart.head} style = {homePageMoobie.moobie_head}/>
-            <Image source = {bodyPart.body} style = {homePageMoobie.moobie_body}/>
-            <Image source = {bodyPart.lowerBody} style = {homePageMoobie.moobie_feet}/>
-            <Text style = {{bottom: 200, fontSize: 20}}> Honey Coins: {currency} </Text>
-            <Button
-                title = "go to main journal page (temp)"
-                onPress = {() => navigation.navigate('Journal Home Page')}
-            />
-            <Button
-                title = "progress tracking (temp)"
-                onPress = {() => navigation.navigate("Progress Tracking")}
-            />
-            <Button
-                title = "store (temp)"
-                onPress = {() => navigation.navigate("Store Page")}
-            />
-            <Button
-                title = "Closet (temp)"
-                onPress = {() => navigation.navigate("Closet Page")}
-            />
-            <Button
-                title = "go to settings page (temp)"
-                onPress = {() => navigation.navigate("Settings Page")}
-            />
-        </View>
+        // trying to get the background to work but its not working
+        <ImageBackground 
+            source = {require("../imgs/backgrounds/background1.png")} 
+            style = {homePage.background}
+            resizeMode = "cover"
+            >
+            <View style = {styles.container}>
+                <Image source = {bodyPart.head} style = {homePageMoobie.moobie_head}/>
+                <Image source = {bodyPart.body} style = {homePageMoobie.moobie_body}/>
+                <Image source = {bodyPart.lowerBody} style = {homePageMoobie.moobie_feet}/>
+
+                <View style = {homePage.currency}>
+                    <Image 
+                        source = {require("../imgs/honeycoin.png")}
+                        style = {{width: 60, height: 50}}
+                    />
+                    <Text style = {{fontSize: 30, marginTop: 10}}>{currency}</Text>
+                </View>
+                
+                
+                {/* Positions the setting icon */}
+                <View style = {{ position: 'absolute', top: 0, right: 0, paddingTop: 80, paddingRight: 10 }}>
+                    <IconButton
+                        onPress = {() => navigation.navigate("Settings Page")}
+                        iconName = "settings"
+                        iconComponent = {Feather}
+                        size = {30}
+                        color = "black"
+                    />
+                </View>
+                
+                {/* All icons on the bottom bar */}
+                <View style = {homePage.iconBarContainer}>
+                    <View style = {homePage.iconBar}>
+                        <IconButton
+                            onPress = {() => navigation.navigate('Journal Home Page')}
+                            iconName = "pencil-alt"
+                            iconComponent = {FontAwesome5}
+                            size = {30}
+                            color = "black"
+                        />
+                        <IconButton
+                            onPress = {() => navigation.navigate("Progress Tracking")}
+                            iconName = "calendar"
+                            iconComponent = {Entypo}
+                            size = {30}
+                            color = "black"
+                        />
+                        <IconButton
+                            onPress = {() => navigation.navigate("Store Page")}
+                            iconName = "store"
+                            iconComponent = {FontAwesome5}
+                            size = {30}
+                            color = "black"
+                        />
+                        <IconButton
+                            onPress = {() => navigation.navigate("Closet Page")}
+                            iconName = "hanger"
+                            iconComponent = {MaterialCommunityIcons}
+                            size = {30}
+                            color = "black"
+                        />
+                    </View>
+                </View>
+            </View>
+        </ImageBackground>
     );
 };
+
+
+
+export const IconButton = ({ onPress, iconName, iconComponent: IconComponent, size, color }) => {
+    return (
+      <TouchableOpacity onPress={onPress}>
+        <IconComponent name={iconName} size={size} color={color} />
+      </TouchableOpacity>
+    );
+  };
+  
+ 
