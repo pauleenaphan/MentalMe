9d741e93b1844681e605b34c22272862ee6266e6
pauleena phan <pauleena2002@gmail.com>
2024-03-19 17:30:25 -0700

    added honey comb to homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { View, Text, Button, Image } from "react-native"
+import { useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { homePageMoobie, styles } from "./styles.js";
 import { getMoobie } from "./moobie.js";
-import { useFocusEffect } from "@react-navigation/native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getCurrency } from "./currency.js";
 
 //Main home page 
 export const HomePage = ({navigation}) =>{
     const {bodyPart, handlePart} = getMoobie();
+    const {currency, updateCurrency} = getCurrency();
 
     const setBody = async () => {
         try {
@@ -42,6 +44,7 @@ export const HomePage = ({navigation}) =>{
             <Image source = {bodyPart.head} style = {homePageMoobie.moobie_head}/>
             <Image source = {bodyPart.body} style = {homePageMoobie.moobie_body}/>
             <Image source = {bodyPart.lowerBody} style = {homePageMoobie.moobie_feet}/>
+            <Text style = {{bottom: 200, fontSize: 20}}> Honey Coins: {currency} </Text>
             <Button
                 title = "go to main journal page (temp)"
                 onPress = {() => navigation.navigate('Journal Home Page')}
