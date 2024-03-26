01c8fb9b5fffccacbbba9b922f624a83b36336dc
pauleena phan <pauleena2002@gmail.com>
2024-03-26 14:26:28 -0700

    added all imgs and price

--- src/images.js
+++ src/images.js
@@ -1,3 +1,4 @@
+//items are in order by price
 export const images = {
     defaultImgs:[
         {name: 'Default Head', image: require('../imgs/moobie_head/head1.png'), imageP: ('../imgs/moobie_head/head1.png')},
@@ -5,31 +6,39 @@ export const images = {
         {name: 'Default Lower Body', image: require('../imgs/moobie_feet/feet1.png'), imageP:('../imgs/moobie_feet/feet1.png')},
     ],
     headImgs: [
-        {name: 'Detective Hat', image: require('../imgs/moobie_head/head2.png'), imageP: ('../imgs/moobie_head/head2.png'), price: '2'},
-        {name: 'Wicked Glasses', image: require('../imgs/moobie_head/head3.png'), imageP: ('../imgs/moobie_head/head3.png'), price: '10'},
-        {name: 'Shades', image: require('../imgs/moobie_head/head4.png'), imageP: ('../imgs/moobie_head/head4.png4'), price: '5'},
         {name: 'HoneyComb Ears', image: require('../imgs/moobie_head/head5.png'), imageP: ('../imgs/moobie_head/head5.png'), price: '0'},
-        {name: 'Flower Head', image: require('../imgs/moobie_head/head6.png'), imageP: ('../imgs/moobie_head/head6.png'), price: '0'},
-        {name: 'Melon Head', image: require('../imgs/moobie_head/head7.png'), imageP: ('../imgs/moobie_head/head7.png'), price: '0'},
-        {name: 'Keycap Hat', image: require('../imgs/moobie_head/head8.png'), imageP: ('../imgs/moobie_head/head8.png'), price: '0'},
-        {name: 'Top Hat', image: require('../imgs/moobie_head/head9.png'), imageP: ('../imgs/moobie_head/head9.png'), price: '0'},
-        {name: 'Old Moobie', image: require('../imgs/moobie_head/head10.png'), imageP: ('../imgs/moobie_head/head10.png'), price: '0'},
-        {name: 'Minion Glasses', image: require('../imgs/moobie_head/head11.png'), imageP: ('../imgs/moobie_head/head11.png'), price: '0'}
+        {name: 'Old Moobie', image: require('../imgs/moobie_head/head10.png'), imageP: ('../imgs/moobie_head/head10.png'), price: '4'},
+        {name: 'Minion Glasses', image: require('../imgs/moobie_head/head11.png'), imageP: ('../imgs/moobie_head/head11.png'), price: '4'},
+        {name: 'Flower Head', image: require('../imgs/moobie_head/head6.png'), imageP: ('../imgs/moobie_head/head6.png'), price: '4'},
+        {name: 'Detective Hat', image: require('../imgs/moobie_head/head2.png'), imageP: ('../imgs/moobie_head/head2.png'), price: '6'},
+        {name: 'Melon Head', image: require('../imgs/moobie_head/head7.png'), imageP: ('../imgs/moobie_head/head7.png'), price: '6'},
+        {name: 'Keycap Hat', image: require('../imgs/moobie_head/head8.png'), imageP: ('../imgs/moobie_head/head8.png'), price: '6'},
+        {name: 'Top Hat', image: require('../imgs/moobie_head/head9.png'), imageP: ('../imgs/moobie_head/head9.png'), price: '6'},
+        {name: 'Shades', image: require('../imgs/moobie_head/head4.png'), imageP: ('../imgs/moobie_head/head4.png4'), price: '10'},
+        {name: 'Wicked Glasses', image: require('../imgs/moobie_head/head3.png'), imageP: ('../imgs/moobie_head/head3.png'), price: '15'},
     ],
     bodyImgs: [
-        {name: 'Gold Chain', image: require('../imgs/moobie_body/body2.png'), imageP: ('../imgs/moobie_body/body2.png'), price: '0'},
-        {name: 'HILHIS Shirt', image: require('../imgs/moobie_body/body3.png'), imageP: ('../imgs/moobie_body/body3.png'), price: '0'},
-        {name: 'Glow Stick Fit', image: require('../imgs/moobie_body/body4.png'), imageP: ('../imgs/moobie_body/body4.png'), price: '0'},
-        {name: 'Frog Shirt', image: require('../imgs/moobie_body/body5.png'), imageP: ('../imgs/moobie_body/body5.png'), price: '0'},
-        {name: 'Cloud Shirt', image: require('../imgs/moobie_body/body6.png'), imageP: ('../imgs/moobie_body/body6.png'), price: '0'},
-        {name: 'Cactus Suit', image: require('../imgs/moobie_body/body7.png'), imageP: ('../imgs/moobie_body/body7.png'), price: '0'},
-        {name: 'Rubber Duck Float', image: require('../imgs/moobie_body/body8.png'), imageP: ('../imgs/moobie_body/body8.png'), price: '0'},
-        {name: 'Minion Overalls', image: require('../imgs/moobie_body/body9.png'), imageP: ('../imgs/moobie_body/body9.png'), price: '0'}
+        {name: 'Rubber Duck Float', image: require('../imgs/moobie_body/body8.png'), imageP: ('../imgs/moobie_body/body8.png'), price: '4'},
+        {name: 'Gold Chain', image: require('../imgs/moobie_body/body2.png'), imageP: ('../imgs/moobie_body/body2.png'), price: '6'},
+        {name: 'Glow Stick Fit', image: require('../imgs/moobie_body/body4.png'), imageP: ('../imgs/moobie_body/body4.png'), price: '6'},
+        {name: 'Frog Shirt', image: require('../imgs/moobie_body/body5.png'), imageP: ('../imgs/moobie_body/body5.png'), price: '6'},
+        {name: 'Cloud Shirt', image: require('../imgs/moobie_body/body6.png'), imageP: ('../imgs/moobie_body/body6.png'), price: '6'},
+        {name: 'Minion Overalls', image: require('../imgs/moobie_body/body9.png'), imageP: ('../imgs/moobie_body/body9.png'), price: '10'},
+        {name: 'Cactus Suit', image: require('../imgs/moobie_body/body7.png'), imageP: ('../imgs/moobie_body/body7.png'), price: '10'},
+        {name: 'HILHIS Shirt', image: require('../imgs/moobie_body/body3.png'), imageP: ('../imgs/moobie_body/body3.png'), price: '15'},
+        
     ],
     lowerBodyImgs: [
-        {name: 'Bunny Slippers', image: require('../imgs/moobie_feet/feet2.png'), imageP:('../imgs/moobie_feet/feet2.png'), price: '0'},
-        {name: 'Pickle Board', image: require('../imgs/moobie_feet/feet3.png'), imageP:('../imgs/moobie_feet/feet3.png'), price: '0'},
-        {name: 'Melon Nikes', image: require('../imgs/moobie_feet/feet4.png'), imageP:('../imgs/moobie_feet/feet4.png'), price: '0'}
+        {name: 'Bunny Slippers', image: require('../imgs/moobie_feet/feet2.png'), imageP:('../imgs/moobie_feet/feet2.png'), price: '4'},
+        {name: 'Flower Shoes', image: require('../imgs/moobie_feet/feet9.png'), imageP:('../imgs/moobie_feet/feet9.png'), price: '4'},
+        {name: 'Melon Nikes', image: require('../imgs/moobie_feet/feet4.png'), imageP:('../imgs/moobie_feet/feet4.png'), price: '6'},
+        {name: 'Swift Shoes', image: require('../imgs/moobie_feet/feet5.png'), imageP:('../imgs/moobie_feet/feet5.png'), price: '6'},
+        {name: 'Glow Wrappers', image: require('../imgs/moobie_feet/feet7.png'), imageP:('../imgs/moobie_feet/feet7.png'), price: '6'},
+        {name: 'Clown Shoes', image: require('../imgs/moobie_feet/feet6.png'), imageP:('../imgs/moobie_feet/feet6.png'), price: '10'},
+        {name: 'Honey Slides', image: require('../imgs/moobie_feet/feet8.png'), imageP:('../imgs/moobie_feet/feet8.png'), price: '10'},
+        {name: 'RGB Sandals', image: require('../imgs/moobie_feet/feet10.png'), imageP:('../imgs/moobie_feet/feet10.png'), price: '10'},
+        {name: 'Pickle Board', image: require('../imgs/moobie_feet/feet3.png'), imageP:('../imgs/moobie_feet/feet3.png'), price: '10'},
+        {name: 'Purple Crocs', image: require('../imgs/moobie_feet/feet11.png'), imageP:('../imgs/moobie_feet/feet11.png'), price: '15'}
     ]
 
 };
