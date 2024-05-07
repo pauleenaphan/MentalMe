629e6f7084b3ba9b367a25ff5644ffebb52fb600
pauleena phan <pauleena2002@gmail.com>
2024-05-06 21:54:54 -0700

    added timer

--- src/cover.js
+++ src/cover.js
@@ -5,17 +5,16 @@ import { coverPage } from "./styles";
 
 export const CoverPage = ({navigation, isLogged}) =>{
     
-    // useEffect(()=>{
-    //     const timer = setTimeout(()=>{
-    //         if(isLogged){
-    //             navigation.navigate("Home Page");
-    //         }else{
-    //             navigation.navigate("Login Page");
-    //         }
-            
-    //     }, 5000)
-    //     return () => clearTimeout(timer);
-    // })
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            if(isLogged){
+                navigation.navigate("Home Page");
+            }else{
+                navigation.navigate("Login Page");
+            }
+        }, 5000)
+        return () => clearTimeout(timer);
+    })
 
     return(
         <View style = {coverPage.container}>
